d653e931c2f8a3c0a596286127e605ed38727156
Francesco <todinofrancesco97@gmail.com>
2023-06-01 10:05:50 +0200

    3 graph in 1 figure, removed 0 values

--- logs/graph.py
+++ logs/graph.py
@@ -18,24 +18,33 @@ for dataset in datasets:
     macro_f1 = [float(row[5]) if row[5] != '' else 0 for row in data]
 
 
+    fig, axs = plt.subplots(1, 3)
+
     x1_data = []
     for i in range(6):
         x1_data += [(2**i) * [2**i]]
     x1_data = [el for sublist in x1_data for el in sublist]
-    x2_data = [1,2,4,8,16,32]
+    x2_data = [1, 2, 4, 8, 16, 32]
 
     for seed in range(0, 3):
         y1_indexes = list([1+(seed*2)] + [i+seed*2+seed for i in range(7, 9)] + [i+seed*4+seed for i in range(16, 20)] + [i+seed*8+seed for i in range(31, 39)] + [i+seed*16+seed for i in range(58, 74)] + [i+seed*32+seed for i in range(109, 141)])
-        y1_data = [accuracy[i] for i in y1_indexes]
+        y1_data = [accuracy[i] for i in y1_indexes if accuracy[i] != 0]
         y2_indexes = list([(seed*2)] + [6+seed*2+seed] + [15+seed*4+seed] + [30+seed*8+seed] + [57+seed*16+seed] + [108+seed*32+seed])
-        y2_data = [accuracy[i] for i in y2_indexes]
+        y2_data = [accuracy[i] for i in y2_indexes if accuracy[i] != 0]
 
-        plt.plot(x1_data, y1_data,'o' ,markersize=4, color = 'blue')
-        plt.plot(x2_data, y2_data, markersize=3, color='green')
+        x1_data = [x1_data[i] for i in range(len(y1_data))]
+        x2_data = [x2_data[i] for i in range(len(y2_data))]
 
-        plt.grid()
-        plt.title(dataset + ' - Seed ' + str(seed+1))
-        plt.xlabel('Prompts')
-        plt.xticks(x2_data, x2_data)
-        plt.ylabel('Accuracy')
-        plt.show()
+        # Aggiungi i grafici agli assi corrispondenti
+        axs[seed].plot(x1_data, y1_data, 'o', markersize=4, color='blue')
+        axs[seed].plot(x2_data, y2_data, markersize=3, color='green')
+
+        axs[seed].grid()
+        axs[seed].set_title(dataset + ' - Seed ' + str(seed+1))
+        axs[seed].set_xlabel('Prompts')
+        axs[seed].set_xticks(x2_data)
+        axs[seed].set_ylabel('Accuracy')
+
+    # Mostra la figura con tutti i grafici
+    plt.tight_layout()
+    plt.show()
