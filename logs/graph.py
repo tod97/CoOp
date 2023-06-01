da17f8289e57fceb0eee5ab23a603b6b759f08f2
Francesco <todinofrancesco97@gmail.com>
2023-06-01 09:18:26 +0200

    dynamic graph production

--- logs/graph.py
+++ logs/graph.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
 import numpy as np
 
-seed = 0
-
 with open('eurosat.csv', 'r') as file:
    reader = csv.reader(file, delimiter=' ')
    data = list(reader)
@@ -23,22 +21,17 @@ for i in range(6):
 x1_data = [el for sublist in x1_data for el in sublist]
 x2_data = [1,2,4,8,16,32]
 
-y1_data = [accuracy[1], accuracy[7], accuracy[8], accuracy[16], accuracy[17], accuracy[18], accuracy[19], accuracy[31], accuracy[32], accuracy[33], accuracy[34], accuracy[35], accuracy[36], accuracy[37], accuracy[38], accuracy[58], accuracy[59], accuracy[60], accuracy[61], accuracy[62], accuracy[63], accuracy[64], accuracy[65], accuracy[66], accuracy[67], accuracy[68], accuracy[69], accuracy[70], accuracy[71], accuracy[72], accuracy[73], accuracy[109], accuracy[110], accuracy[111], accuracy[112], accuracy[113], accuracy[114], accuracy[115], accuracy[116], accuracy[117], accuracy[118], accuracy[119], accuracy[120], accuracy[121], accuracy[122], accuracy[123], accuracy[124], accuracy[125], accuracy[126], accuracy[127], accuracy[128], accuracy[129], accuracy[130], accuracy[131], accuracy[132], accuracy[133], accuracy[134], accuracy[135], accuracy[136], accuracy[137], accuracy[138], accuracy[139], accuracy[140]]
-y2_data = [accuracy[0], accuracy[6], accuracy[15], accuracy[30], accuracy[57], accuracy[108]]
+for seed in range(0, 3):
+    y1_indexes = list([1+(seed*2)] + [i+seed*2+seed for i in range(7, 9)] + [i+seed*4+seed for i in range(16, 20)] + [i+seed*8+seed for i in range(31, 39)] + [i+seed*16+seed for i in range(58, 74)] + [i+seed*32+seed for i in range(109, 141)])
+    y1_data = [accuracy[i] for i in y1_indexes]
+    y2_indexes = list([(seed*2)] + [6+seed*2+seed] + [15+seed*4+seed] + [30+seed*8+seed] + [57+seed*16+seed] + [108+seed*32+seed])
+    y2_data = [accuracy[i] for i in y2_indexes]
 
+    plt.plot(x1_data, y1_data,'o' ,markersize=4, color = 'blue')
+    plt.plot(x2_data, y2_data, markersize=3, color='green')
 
-# Visualizzazione
-#x1_data.sort()
-#y1_data.sort()
-#x2_data.sort()
-#y2_data.sort()
-
-
-plt.plot(x1_data, y1_data,'o' ,markersize=4, color = 'black')
-plt.plot(x2_data, y2_data, markersize=3, color='green')
-
-plt.grid()
-plt.title('Data points')
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
-plt.show()
+    plt.grid()
+    plt.title('Data points')
+    plt.xlabel('x-axis')
+    plt.ylabel('y-axis')
+    plt.show()
